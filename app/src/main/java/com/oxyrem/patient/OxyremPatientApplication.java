dc9ce3481a8586bfc46cfc574752120a192976f4
bfranklyn <bfranklyn11@hotmail.com>
2015-03-26 01:25:58 -0500

    Added firebase dependencies and jar

--- app/src/main/java/com/oxyrem/patient/OxyremPatientApplication.java
+++ app/src/main/java/com/oxyrem/patient/OxyremPatientApplication.java
@@ -2,10 +2,18 @@ package com.oxyrem.patient;
 
 import android.app.Application;
 
+import com.firebase.client.Firebase;
+
 /**
  * Oxyrem:Patient Application class.
  * @author Patrick Reid
  * @see android.app.Application
  */
 public class OxyremPatientApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Firebase.setAndroidContext(this);
+    }
 }
